cc90b0dc89f7a78a4d0d6e4e0047745d82c1c9b0
Paul Miranda Vega <paul.miranda@uabc.edu.mx>
2019-09-04 08:56:20 -0700

    History modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import { compose, createStore, applyMiddleware } from 'redux';
-import {Router} from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
@@ -16,9 +16,9 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    <Provider store={store} basename={process.env.PUBLIC_URL}>
+    <Provider store={store}>
         <Router history={history}>
-            <App/>
+            <App />
         </Router>
     </Provider>,
     document.querySelector('#root')
